fe3b7c1e78a7e589ad2fa4dfdde6691c626a274a
Kevin Thunert <kevin.thunert@live.de>
2022-12-05 00:04:28 +0100

    part2 initial solution

---
+++ day1/day1_2.py
@@ -0,0 +1,19 @@
+# https://adventofcode.com/2022/day/1
+import os
+
+#gets input file on every OS
+script_dir = os.path.dirname(__file__) #abs script directory
+rel_path = "input.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path, "r") as f:
+    listOfElves = []
+    totalCalories = 0
+    for l in f.readlines():
+        if l == "\n":
+            listOfElves.append(totalCalories)
+            totalCalories = 0
+            continue
+        totalCalories += int(l)
+    listOfElves.sort(reverse=True)
+    print(listOfElves[0]+listOfElves[1]+listOfElves[2])
